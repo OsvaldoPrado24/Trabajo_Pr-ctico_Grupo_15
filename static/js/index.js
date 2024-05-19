88a74296737410159d85ff80648c2fcef8369f27
Maria Eugenia <marueu12@hotmail.com>
2024-05-18 22:12:52 -0300

    formulario validado

--- static/js/index.js
+++ static/js/index.js
@@ -1,28 +1,57 @@
-// scripts.js
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    let formValid = true;
+// Función validar formulario
+function validarDatos() {
 
-    // Validar campos obligatorios
-    document.querySelectorAll('#contactForm [required]').forEach(function(element) {
-        if (!element.value) {
-            formValid = false;
-            element.style.border = '1px solid red';
-        } else {
-            element.style.border = '1px solid #ccc';
-        }
-    });
+    let telefono = document.getElementById("telefono").value.trim()
+    let error = document.getElementById("campos")
 
-    // Validar checkbox de términos
-    let terms = document.getElementById('terms');
-    if (!terms.checked) {
+    
+    // Verificar si el nombre contiene solo tiene caracteres alfabéticos y mas de 3 letras   
+    const nameInput = document.getElementById('nombre');
+    const namePattern = /^[a-zA-Z\s]+$/;
+    const trimmedName = nameInput.value.trim();
+    if (!namePattern.test(trimmedName) || trimmedName.length <= 2) {
         formValid = false;
-        terms.nextElementSibling.style.color = 'red';
+        nameInput.style.border = '1px solid red';
+        error.textContent = "Por favor ingrese un nombre válido que contenga solo letras  y más de 3 caracteres."
+
+       return false
     } else {
-        terms.nextElementSibling.style.color = 'black';
+        nameInput.style.border = '1px solid #ccc';
+    }
+// Verificar si el apellido contiene solo tiene caracteres alfabéticos y mas de 3 letras
+    const apellidoInput = document.getElementById('apellido');
+    const apellidoPattern = /^[a-zA-Z\s]+$/;
+    const trimapellido = apellidoInput.value.trim();
+    if (!apellidoPattern.test(trimapellido) || trimapellido.length <= 2) {
+        formValid = false;
+        nameInput.style.border = '1px solid red';
+        error.textContent = "Por favor ingrese un apellido válido que contenga solo letras  y más de 3 caracteres."
+
+       return false
+    } else {
+        nameInput.style.border = '1px solid #ccc';
+    }
+// Validar correo electrónico
+const emailInput = document.getElementById('email');
+const emailPattern = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
+if (!emailPattern.test(emailInput.value)) {
+    formValid = false;
+    emailInput.style.border = '1px solid red';
+    error.textContent="Por favor, ingrese una dirección de correo electrónico válida."
+    return false
+} else {
+    emailInput.style.border = '1px solid #ccc';
+}
+    // Verificar tel no es un número
+    if (isNaN(telefono)) {
+        error.textContent = "Por favor ingrese solo números."
+        error.style.color = "brown"
+        return false
     }
 
-    if (!formValid) {
-        event.preventDefault();
-        alert('Por favor, complete todos los campos obligatorios.');
-    }
-});
+    // Si todas las validaciones son exitosas, informar
+    alert("Datos validados.")
+    return true
+
+}
+
